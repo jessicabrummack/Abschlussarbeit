9c0b28b7382ee7193c01e3a542573118221bfb6b
jessicabrummack <brummjes@b-tu.de>
2023-05-25 18:30:20 +0200

    commit

--- docs/script.js
+++ docs/script.js
@@ -1,3 +1,5 @@
+<!--function to open the hamburger menu-->
+
 hamburger = document.querySelector(".hamburger");
 hamburger.onclick = function() {
     navLinks = document.querySelector(".nav-links");
