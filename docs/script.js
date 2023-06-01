a0529e725dc7f6cc83bcf0f414867c561a5b5e80
jessicabrummack <brummjes@b-tu.de>
2023-06-01 11:03:39 +0200

    commit

--- docs/script.js
+++ docs/script.js
@@ -5,3 +5,10 @@ hamburger.onclick = function() {
     navLinks = document.querySelector(".nav-links");
     navLinks.classList.toggle("active");
 }
+
+sidebar = document.querySelector(".sidebar");
+sidebar.onclick = function() {
+    menu_toggle = document.querySelector(".left-sidebar");
+    menu_toggle.classList.toggle("active");
+
+}
